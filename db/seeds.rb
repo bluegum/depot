79937bb9caed2463f8ec3ed26e1ab769867de632
Dakai <dliu@toshiba-tap.com>
2013-06-28 15:02:52 +1000

    Added image in Product View

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+#
+Product.create(title: 'FooBar', description: 'Full Metal Jacket', image_url: 'foobar.png', price: 999.95)
+#
+Product.create(title: 'SilverBullet', description:
+               %{<p>
+ Must have for vampire slayers </p>},
+               image_url: 'bullet.jpg', price: 9.95)
+#
+Product.create(title: 'Dragon', description:
+               %{<p>
+ Powerful magical creature. </p>},
+               image_url: 'dragon.png', price: 999.95)
