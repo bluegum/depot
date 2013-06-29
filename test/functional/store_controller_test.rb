147bcc824435ffa370cf327e75850d080f207f49
Dakai Liu <dakailiu.os@gmail.com>
2013-06-29 13:21:46 +1000

    Added Store front controller and updated tests

---
+++ test/functional/store_controller_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class StoreControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+
+    assert_select 'h3', 'Dragon'
+    assert_select '.price', /\$[,\d]+\.\d\d/
+  end
+
+end
